c220266649c4a23e0ec443575c2dc559e6b6a9d0
YuetongFang <51812852+0x01-kelly@users.noreply.github.com>
2021-07-26 14:48:12 +0800

    Update gpio_config.h

--- gpio_config.h
+++ gpio_config.h
@@ -1,7 +1,7 @@
 /*
  * gpio.h
  *
- *  Created on: 2021Äê7ÔÂ11ÈÕ
+ *  Created on: 2021Ã„Ãª7Ã”Ã‚11ÃˆÃ•
  *      Author: FYT
  *      for all the pin definitions and configurations
  */
@@ -18,39 +18,6 @@
  *
  */
 
-
-/*****OLED******
- * P4.2 -- SCL
- * P4.1 -- SDA
- **************/
-#define OLED_GPIO_CONFIG()  P4DIR |=  BIT1+BIT2
-#define OLED_SCLK_Clr()     P4OUT &=~ BIT2  //SCL
-#define OLED_SCLK_Set()     P4OUT |=  BIT2
-
-#define OLED_SDIN_Clr()     P4OUT &=~ BIT1  //SDA
-#define OLED_SDIN_Set()     P4OUT |=  BIT1
-
-/*****KEYBOARD******
- * ROW0-3: P8.5, P9.0, P8.4, P8.2
- * COW0-3: P8.6, P8.7, P9.1, P8.3
- **************/
-//the structure for Key_defines
-struct IO_port{
-      uint8_t  GPIO_PORT_PX;
-      uint16_t GPIO_PINX;};
-//PIN DEFIN
-//row0-3
-static struct IO_port  Key_output[]={{GPIO_PORT_P8,GPIO_PIN5},
-                                     {GPIO_PORT_P9,GPIO_PIN0},
-                                     {GPIO_PORT_P8,GPIO_PIN4},
-                                     {GPIO_PORT_P8,GPIO_PIN2}};
-//col0-3
-static struct IO_port  Key_input[]={ {GPIO_PORT_P8,GPIO_PIN6},
-                                     {GPIO_PORT_P8,GPIO_PIN7},
-                                     {GPIO_PORT_P9,GPIO_PIN1},
-                                     {GPIO_PORT_P8,GPIO_PIN3}};
-//PIN CONFIGs
-void Key_Init();
 
 
 
