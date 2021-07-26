9c6b21627a99018c191f4585a2d414d35008a1d8
YuetongFang <51812852+0x01-kelly@users.noreply.github.com>
2021-07-26 14:48:41 +0800

    Update gpio_config.c

--- gpio_config.c
+++ gpio_config.c
@@ -1,7 +1,7 @@
 /*
  * gpio_config.c
  *
- *  Created on: 2021Äê7ÔÂ24ÈÕ
+ *  Created on: 2021Ã„Ãª7Ã”Ã‚24ÃˆÃ•
  *      Author: FYT
  *      for all the pin definitions and configurations
  */
@@ -9,21 +9,3 @@
 #include "gpio_config.h"
 
 
-void Key_Init(){
-    MAP_GPIO_setAsOutputPin(Key_output[0].GPIO_PORT_PX,Key_output[0].GPIO_PINX);
-    MAP_GPIO_setAsOutputPin(Key_output[1].GPIO_PORT_PX,Key_output[1].GPIO_PINX);
-    MAP_GPIO_setAsOutputPin(Key_output[2].GPIO_PORT_PX,Key_output[2].GPIO_PINX);
-    MAP_GPIO_setAsOutputPin(Key_output[3].GPIO_PORT_PX,Key_output[3].GPIO_PINX);
-
-    MAP_GPIO_setDriveStrengthLow(Key_output[0].GPIO_PORT_PX,Key_output[0].GPIO_PINX);
-    MAP_GPIO_setDriveStrengthLow(Key_output[1].GPIO_PORT_PX,Key_output[1].GPIO_PINX);
-    MAP_GPIO_setDriveStrengthLow(Key_output[2].GPIO_PORT_PX,Key_output[2].GPIO_PINX);
-    MAP_GPIO_setDriveStrengthLow(Key_output[3].GPIO_PORT_PX,Key_output[3].GPIO_PINX);
-
-    MAP_GPIO_setAsInputPinWithPullUpResistor(Key_input[0].GPIO_PORT_PX,Key_input[0].GPIO_PINX);
-    MAP_GPIO_setAsInputPinWithPullUpResistor(Key_input[1].GPIO_PORT_PX,Key_input[1].GPIO_PINX);
-    MAP_GPIO_setAsInputPinWithPullUpResistor(Key_input[2].GPIO_PORT_PX,Key_input[2].GPIO_PINX);
-    MAP_GPIO_setAsInputPinWithPullUpResistor(Key_input[3].GPIO_PORT_PX,Key_input[3].GPIO_PINX);
-
-
-}
